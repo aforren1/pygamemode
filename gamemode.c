a3ddd15af568e32fba56d8336286c5a3a99d5fbd
Alex Forrence <aforren1@jhu.edu>
2020-02-05 11:38:07 -0500

    add remaining methods, full test

--- gamemode.c
+++ gamemode.c
@@ -2,7 +2,7 @@
 #include "gamemode_client.h"
 
 PyDoc_STRVAR(request_start__doc__,
-             "int gamemode_request_start() - Request gamemode starts\n"
+             "int request_start() - Request gamemode starts\n"
              "    0 if the request was sent successfully\n"
              "   -1 if the request failed\n");
 
@@ -12,7 +12,7 @@ static PyObject *request_start(PyObject *self, PyObject *noargs)
 }
 
 PyDoc_STRVAR(request_end__doc__,
-             "int gamemode_request_end() - Request gamemode ends\n"
+             "int request_end() - Request gamemode ends\n"
              "    0 if the request was sent successfully\n"
              "   -1 if the request failed\n");
 
@@ -22,7 +22,7 @@ static PyObject *request_end(PyObject *self, PyObject *noargs)
 }
 
 PyDoc_STRVAR(query_status__doc__,
-             "int gamemode_query_status() - Query the current status of gamemode\n"
+             "int query_status() - Query the current status of gamemode\n"
              "    0 if gamemode is inactive\n"
              "    1 if gamemode is active\n"
              "    2 if gamemode is active and this client is registered\n"
@@ -32,10 +32,59 @@ static PyObject *query_status(PyObject *self, PyObject *noargs)
     return PyLong_FromLong(gamemode_query_status());
 }
 
+PyDoc_STRVAR(request_start_for__doc__,
+             "int request_start_for(int pid) - Request gamemode starts for another process\n"
+             "   0 if the request was sent successfully\n"
+             "  -1 if the request failed\n"
+             "  -2 if the request was rejected\n");
+static PyObject *request_start_for(PyObject *self, PyObject *args)
+{
+    int pid;
+    if (!PyArg_ParseTuple(args, "i", &pid))
+    {
+        return NULL;
+    }
+    return PyLong_FromLong(gamemode_request_start_for(pid));
+}
+
+PyDoc_STRVAR(request_end_for__doc__,
+             "int request_end_for(int pid) - Request gamemode ends for another process\n"
+             "   0 if the request was sent successfully\n"
+             "   -1 if the request failed\n"
+             "   -2 if the request was rejected\n");
+static PyObject *request_end_for(PyObject *self, PyObject *args)
+{
+    int pid;
+    if (!PyArg_ParseTuple(args, "i", &pid))
+    {
+        return NULL;
+    }
+    return PyLong_FromLong(gamemode_request_end_for(pid));
+}
+
+PyDoc_STRVAR(query_status_for__doc__,
+             "int query_status_for(pid_t pid) - Query status of gamemode for another process\n"
+             "   0 if gamemode is inactive\n"
+             "   1 if gamemode is active\n"
+             "   2 if gamemode is active and this client is registered\n"
+             "   -1 if the query failed\n");
+static PyObject *query_status_for(PyObject *self, PyObject *args)
+{
+    int pid;
+    if (!PyArg_ParseTuple(args, "i", &pid))
+    {
+        return NULL;
+    }
+    return PyLong_FromLong(gamemode_query_status_for(pid));
+}
+
 static PyMethodDef gamemode_meths[] = {
     {"request_start", request_start, METH_NOARGS, request_start__doc__},
     {"request_end", request_end, METH_NOARGS, request_end__doc__},
     {"query_status", query_status, METH_NOARGS, query_status__doc__},
+    {"request_start_for", request_start_for, METH_VARARGS, request_start_for__doc__},
+    {"request_end_for", request_end_for, METH_VARARGS, request_end_for__doc__},
+    {"query_status_for", query_status_for, METH_VARARGS, query_status_for__doc__},
     {NULL, NULL, 0, NULL}};
 
 static struct PyModuleDef gamemodemodule = {
---
+++ test.py
@@ -0,0 +1,16 @@
+import os
+import gamemode as gm
+
+pid = os.getpid()
+
+print(gm.request_start())
+
+print(gm.query_status())
+
+print(gm.request_end())
+
+print(gm.request_start_for(pid))
+
+print(gm.query_status_for(pid))
+
+print(gm.request_end_for(pid))
