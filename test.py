cc210ec8de6d8d2a381a1095a59fe3348403ddff
Alex Forrence <aforren1@jhu.edu>
2020-02-05 17:19:54 -0500

    formatting

--- test.py
+++ test.py
@@ -4,13 +4,10 @@ import gamemode as gm
 pid = os.getpid()
 
 print(gm.request_start())
-
 print(gm.query_status())
-
 print(gm.request_end())
 
 print(gm.request_start_for(pid))
-
 print(gm.query_status_for(pid))
-
 print(gm.request_end_for(pid))
+
